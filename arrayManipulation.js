a8f365f52c6b0e3ee9fb6c8f7f6b010b2e80739e
bsafo84 <cadirosafo@gmail.com>
2024-05-19 01:10:43 +0000

    task 2 complete

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -9,6 +9,16 @@ function processArray(numbers) {
   });
 }
 
+function formatArrayStrings(strings, numbers) {
+  return strings.map(function (str, index) {
+    if (numbers[index] % 2 === 0){
+      return str.toUpperCase();
+    } else {
+      return str.toLowerCase();
+    }
+  });
+}
+
 
 const numbers = [1, 2, 3, 4, 5];
 const result = processArray(numbers);
